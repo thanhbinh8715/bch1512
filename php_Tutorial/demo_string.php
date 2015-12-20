052a59820c5e84496f187f69ae1c7e6a5cf9508b
thanhbinh <thanhbinh@binhgiang-pc>
2015-12-20 09:22:35 +0700

    vi du string

--- php_Tutorial/demo_string.php
+++ php_Tutorial/demo_string.php
@@ -5,3 +5,5 @@ echo "<br> Do dai chuoi la : " . strlen($ten);
 echo "<br> So ky tu ten lop cua chung ta la : ". strlen('BCH1512');
 echo "<br> So tu trong ten cua minh la :" . str_word_count($ten);
 echo "<br> So tu trong chuoi \"xin chao cac ban\" la : " . str_word_count('xin chao cac ban');
+echo "<br> Chuoi dao nguoc ten cua Minh la : " . strrev($ten);
+echo "<br> Chuoi dao nguoc cua 'xin chao cac ban' la: " .strrev('xin chao cac ban');
