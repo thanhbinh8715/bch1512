b469b4add800ed8c577fe3626d03bba71fc0e8ef
thanhbinh <thanhbinh@binhgiang-pc>
2015-12-20 09:19:38 +0700

    vi du string

--- php_Tutorial/demo_string.php
+++ php_Tutorial/demo_string.php
@@ -1,5 +1,7 @@
 <?php
 
-$ten = "Vu Thanh Binh";
+$ten = "Vu,Thanh.Binh";
 echo "<br> Do dai chuoi la : " . strlen($ten);
 echo "<br> So ky tu ten lop cua chung ta la : ". strlen('BCH1512');
+echo "<br> So tu trong ten cua minh la :" . str_word_count($ten);
+echo "<br> So tu trong chuoi \"xin chao cac ban\" la : " . str_word_count('xin chao cac ban');
