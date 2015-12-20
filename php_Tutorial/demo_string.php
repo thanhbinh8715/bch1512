036cd39b095a169788ad0705402ecce2b64bfdcc
thanhbinh <thanhbinh@binhgiang-pc>
2015-12-20 09:37:29 +0700

    vi du string

--- php_Tutorial/demo_string.php
+++ php_Tutorial/demo_string.php
@@ -8,3 +8,4 @@ echo "<br> So tu trong chuoi \"xin chao cac ban\" la : " . str_word_count('xin c
 echo "<br> Chuoi dao nguoc ten cua Minh la : " . strrev($ten);
 echo "<br> Chuoi dao nguoc cua 'xin chao cac ban' la: " .strrev('xin chao cac ban');
 echo "<br> Tu Thanh o vi tri thu : " . strpos($ten, 'Thanh');
+echo "<br> Ten cua Ban sau khi thay the la : " . str_replace('Vu', 'Phan', $ten);
