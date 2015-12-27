2c917dce8c70c191d321aefcd0bd04c7170904bc
thanhbinh <thanhbinh@binhgiang-pc>
2015-12-27 09:36:13 +0700

    vi du ifelse

--- php_Tutorial/demo_ifelse.php
+++ php_Tutorial/demo_ifelse.php
@@ -1,7 +1,11 @@
 <?php
 date_default_timezone_set('Asia/Ho_Chi_Minh');
-$gio = date("H");
+$gio = date("G");
+$thu = date("D");
+if ($thu == "Sun"){
+    echo "Hom nay la Chu Nhat.<br/>";    
+}
 if ($gio <12){
-    echo "Chao buoi sang ! Bay gio la $gio gio.";
+    echo "Chao buoi sang ! Bay gio la thu ".  date("D H:i:s")." gio.";
 }
 
