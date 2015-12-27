33985dae8dc39f6f3ed973224a4238eccb3723da
thanhbinh <thanhbinh@binhgiang-pc>
2015-12-27 14:20:49 +0700

    vi du ham

--- php_Tutorial/demo_function.php
+++ php_Tutorial/demo_function.php
@@ -4,9 +4,10 @@
 
 hienThiTen("Vu Thanh Binh", 29);
 hienThiTen("Le Minh Hoang", 24);
+hienThiTen("Kieu Mai Mo");
 
 
 
-function hienThiTen($hoten, $tuoi){
+function hienThiTen($hoten, $tuoi = 20){
     echo "<br> Ten cua Ban la : $hoten. Ban co so tuoi la : $tuoi";    
 }
