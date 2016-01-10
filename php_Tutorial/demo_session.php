df30152d4237a349e5b7740375df6ba2c4e08f44
thanhbinh <thanhbinh@binhgiang-pc>
2016-01-10 16:30:55 +0700

    demo session

--- php_Tutorial/demo_session.php
+++ php_Tutorial/demo_session.php
@@ -14,7 +14,13 @@ if (isset($_SESSION['username'])) {
 }
 var_dump($_SESSION);
 
+file_put_contents('data.txt', '1,12,2000');
+$data = file_get_contents('data.txt');
+$mang = explode(',', $data);
+print_r($mang);
+
 ?>
 
 <a href="?act=logout">Log out</a>
 <a href="?act=login">Login </a>
+
